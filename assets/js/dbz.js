0c37e2c98e7679c5e67559f1c4a34fca04da60b2
irisgraser <grafik@irisgraser.ch>
2020-07-13 10:37:34 +0200

    browserblast

--- assets/js/dbz.js
+++ assets/js/dbz.js
@@ -2,6 +2,18 @@
 
 $(document).ready(function(){
 
+  var rhabarber = '<div class="browserblast">';
+    rhabarber += '<h2>:-(</h2>';
+    rhabarber += '<p>Ihr Browser stammt aus geraumer Vorzeit und ist zu alt, um unsere Website sauber darzustellen und sicher zu nutzen.</p>';
+    rhabarber += '<p>Wir empfehlen, ein <a href="https://www.microsoft.com/en-us/edge" target="_blank">Upgrade</a> oder einen Wechsel auf <a href="https://www.firefox.com" target="_blank">Firefox</a> oder <a href="https://www.google.com/chrome" target="_blank">Google Chrome</a> vorzunehmen.</p>';
+    rhabarber += '<p class="absender">Freundliche Grüsse<br>A.Degginger, S.Bischof und S.Zlabinger</p>';
+    rhabarber += '<p class="browserblast__closer">×</p>';
+    rhabarber += '</div>';
+    document.body.innerHTML += rhabarber;
+    $(".browserblast__closer").click(function(){
+      $(".browserblast").fadeOut();
+    });
+
   $(".trigger").click(function(){
   // $(this).toggleClass("offen");
   $(this).siblings(".trigger").next().slideUp();
